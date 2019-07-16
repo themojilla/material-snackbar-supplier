f4fa4fbf5b100a76b54b074c58f2e2ac183c27e4
amerllica <amerllica@gmail.com>
2019-07-16 12:43:36 +0430

    Using destructuring assignment to avoid large names. I believe it causes code be human readable.

--- src/supplier.js
+++ src/supplier.js
@@ -11,40 +11,7 @@ import {
 import { Close as CloseIcon } from '@material-ui/icons';
 import { green, amber } from '@material-ui/core/colors';
 
-
-
-const styles = ({ spacing, palette }) => ({
-  close: {
-    width: spacing(4),
-    height: spacing(4),
-    padding: 0,
-  },
-  success: {
-    backgroundColor: green[600],
-  },
-  error: {
-    backgroundColor: palette.error.dark,
-  },
-  info: {
-    backgroundColor: palette.primary.dark,
-  },
-  warning: {
-    backgroundColor: amber[700],
-  },
-  icon: {
-    fontSize: 20,
-  },
-  iconVariant: {
-    opacity: 0.9,
-    marginRight: spacing(1),
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center',
-  }
-});
-
-const SnackBarContext = createContext();
+const { Consumer, Provider } = createContext(undefined);
 
 export const withSnackBar = Component => props => (
   <SnackBarContext.Consumer>
@@ -103,7 +70,7 @@ class SnackBarSupplier extends PureComponent {
     } = this.props;
 
     return (
-      <SnackBarContext.Provider value={this.getChildrenContext()}>
+      <Provider value={this.getChildrenContext()}>
         {children}
         <NoSsr>
           <Snackbar
@@ -137,7 +104,7 @@ class SnackBarSupplier extends PureComponent {
             />
           </Snackbar>
         </NoSsr>
-      </SnackBarContext.Provider>
+      </Provider>
     )
   }
 }
